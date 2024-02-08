a396d9a3e085e077929ccfefd751c14fb68ef71e
Lucas Cardoso <Luckycards433@gmail.com>
2024-02-08 17:35:59 -0300

    Importação do ChangeEvent

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
