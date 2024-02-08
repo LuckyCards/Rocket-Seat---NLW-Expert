7cd372fbd8f4281596e290dd550fc72e7324dee3
Lucas Cardoso <Luckycards433@gmail.com>
2024-02-08 17:28:46 -0300

    Correção da responsividade do tamanho dos blocos de notas

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -89,7 +89,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger className="rounded-md overflow-hidden  flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-200">
           Adicionar nota
         </span>
